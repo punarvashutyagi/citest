26f44250d943ca4b66561e3f5f9e2b19b5181776
punarvashutyagi <77241988+punarvashutyagi@users.noreply.github.com>
2021-04-12 23:01:05 +0000

    Update GreetingController.java

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	@GetMapping("/bookzy")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="hello bookzy") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
